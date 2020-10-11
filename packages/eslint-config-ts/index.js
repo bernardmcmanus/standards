f645c1d01dc981f5f923def07336e866167d0b1d
Bernard McManus <bernard.mcmanus.iv@gmail.com>
2020-10-11 16:13:41 -0400

    fixes

--- packages/eslint-config-ts/index.js
+++ packages/eslint-config-ts/index.js
@@ -90,6 +90,7 @@ module.exports = {
 			files: ['**/*.js?(x)'],
 			rules: {
 				'@typescript-eslint/explicit-function-return-type': 'off',
+				'@typescript-eslint/explicit-module-boundary-types': 'off',
 				'@typescript-eslint/no-var-requires': 'off',
 			},
 		},
