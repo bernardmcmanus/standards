7d9f84ccd2d9b2673ea3c9f0110da937190bb3d6
Bernard McManus <bernard.mcmanus.iv@gmail.com>
2020-10-24 15:11:35 -0400

    better nodemon config support, default to esm instead of babel for *.js

--- packages/npm-scripts/lib/nodemon.js
+++ packages/npm-scripts/lib/nodemon.js
@@ -1,35 +1,39 @@
 const mergeWith = require('lodash/mergeWith');
 
-let mainConfig;
+let mainConfig = {};
 
 try {
-	mainConfig = require.resolve('./nodemon.json', {
+	const mainConfigPath = require.resolve('./nodemon', {
 		paths: [
 			process.cwd()
 		]
 	});
+	mainConfig = require(mainConfigPath);
 } catch (err) {
 	if (err.code !== 'MODULE_NOT_FOUND') {
 		throw err;
 	}
 }
 
-module.exports = mergeWith(
-	{
-		ext: 'g?(raph)ql js?(on|x) ts?(x) y?(a)ml',
-		watch: '**/*',
-		ignore: ['coverage/', 'dist/', 'node_modules/'],
-		env: {
-			TS_NODE_TRANSPILE_ONLY: 1
-		},
-		execMap: {
-			js: 'node --require @babel/register',
-			jsx: 'node --require @babel/register',
-			ts: 'node --require ts-node/register',
-			tsx: 'node --require ts-node/register'
-		}
+const defaultConfig = {
+	ext: 'g?(raph)ql js?(on|x) ts?(x) y?(a)ml',
+	watch: '**/*',
+	ignore: ['coverage/', 'dist/', 'node_modules/'],
+	env: {
+		TS_NODE_TRANSPILE_ONLY: 1
 	},
-	mainConfig ? require(mainConfig) : {},
+	execMap: {
+		js: 'node -r esm',
+		jsx: 'node -r @babel/register',
+		ts: 'node -r ts-node/register',
+		tsx: 'node -r ts-node/register'
+	}
+};
+
+module.exports = mergeWith(
+	{},
+	defaultConfig,
+	typeof mainConfig === 'function' ? mainConfig({ ...defaultConfig }) : mainConfig,
 	(objValue, srcValue) => {
 		if (Array.isArray(objValue)) {
 			return Array.from(new Set(objValue.concat(srcValue)));
